029179a1520bde999f5e8b94cd553ce6a64b4f19
Rache Melendres <rachemelendres@gmail.com>
2025-06-17 09:17:10 +0000

    fix mypy errors

--- dlt_dbt_dagster/definitions.py
+++ dlt_dbt_dagster/definitions.py
@@ -1,8 +1,9 @@
+from dagster import Definitions
 from dagster.components import definitions, load_defs
 
 import dlt_dbt_dagster.defs
 
 
 @definitions
-def defs():
+def defs() -> Definitions:
     return load_defs(defs_root=dlt_dbt_dagster.defs)
