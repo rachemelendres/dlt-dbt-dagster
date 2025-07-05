a5af2249aabd33ae5d1083047a828270e87c03e5
Rache Melendres <rachemelendres@gmail.com>
2025-07-05 12:38:52 +0000

    include rockets, ships, cores, payloads, and launchpads to rest_api_config

--- dlt_dbt_dagster/dlt/spacex_pipeline.py
+++ dlt_dbt_dagster/dlt/spacex_pipeline.py
@@ -6,7 +6,8 @@ import dlt
 from dlt.sources.rest_api import RESTAPIConfig, rest_api_resources
 from dotenv import load_dotenv
 
-from dlt_dbt_dagster.constants.endpoints import BASE_URL, ENDPOINTS
+from dlt_dbt_dagster.constants.endpoints import BASE_URL, Endpoints
+from dlt_dbt_dagster.constants.schema.bronze import BronzeSchema
 from dlt_dbt_dagster.dlt.custom_paginator import CustomJsonPaginator
 from dlt_dbt_dagster.utils.processing_utils import add_year_month, get_month_range, keep_specific_columns
 
@@ -15,40 +16,11 @@ load_dotenv()
 YEAR = 2021
 MONTH = 3
 
-LAUNCHES_SELECTED_COLUMNS = [
-    "id",
-    "name",
-    "details",
-    "flight_number",
-    "launchpad",
-    "date_utc",
-    "rocket",
-    "payloads",
-    "ships",
-    "cores",
-    "success",
-]
-
-ROCKETS_SELECTED_COLUMNS = [
-    "id",
-    "name",
-    "description",
-    "company",
-    "type",
-    "active",
-    "stages",
-    "boosters",
-    "cost_per_launch",
-    "success_rate_pct",
-    "first_flight",
-    "country",
-]
-
 
 @dlt.source(
     name="spacex_api_source",
     max_table_nesting=0,
-    schema_contract={"tables": "evolve", "columns": "discard_value", "data_type": "freeze"},
+    schema_contract={"tables": "evolve", "columns": "discard_value", "data_type": "evolve"},
 )
 def spacex_api_source(year: int, month: int) -> Any:
     """Make the SpaceX API source"""
@@ -77,26 +49,62 @@ def spacex_api_source(year: int, month: int) -> Any:
                 "merge_key": ["year", "month"],
                 "columns": {"date_utc": {"dedup_sort": "desc"}},
                 "write_disposition": "merge",
-                "name": "bronze_launches",
+                "name": BronzeSchema.LAUNCHES,
                 "endpoint": {
-                    "path": ENDPOINTS["launches"],
+                    "path": Endpoints.LAUNCHES,
                     "json": {
                         "query": {"date_utc": {"$gte": start_date, "$lt": end_date}},
                         "options": {"limit": 50},
                     },
                 },
                 "processing_steps": [
-                    {"map": keep_specific_columns(columns_to_keep=LAUNCHES_SELECTED_COLUMNS)},  # type: ignore[typeddict-item]
+                    {"map": keep_specific_columns(columns_to_keep=BronzeSchema.get_columns(BronzeSchema.LAUNCHES))},  # type: ignore[typeddict-item]
                     {"map": add_year_month(year=year, month=month)},  # type: ignore[typeddict-item]
                 ],
             },
             {
-                "name": "bronze_rockets",
+                "name": BronzeSchema.ROCKETS,
                 "endpoint": {
-                    "path": ENDPOINTS["rockets"],
+                    "path": Endpoints.ROCKETS,
                 },
                 "processing_steps": [
-                    {"map": keep_specific_columns(columns_to_keep=ROCKETS_SELECTED_COLUMNS)},  # type: ignore[typeddict-item]
+                    {"map": keep_specific_columns(columns_to_keep=BronzeSchema.get_columns(BronzeSchema.ROCKETS))},  # type: ignore[typeddict-item]
+                ],
+            },
+            {
+                "name": BronzeSchema.CORES,
+                "endpoint": {
+                    "path": Endpoints.CORES,
+                },
+                "processing_steps": [
+                    {"map": keep_specific_columns(columns_to_keep=BronzeSchema.get_columns(BronzeSchema.CORES))},  # type: ignore[typeddict-item]
+                ],
+            },
+            {
+                "name": BronzeSchema.PAYLOADS,
+                "endpoint": {
+                    "path": Endpoints.PAYLOADS,
+                },
+                "processing_steps": [
+                    {"map": keep_specific_columns(columns_to_keep=BronzeSchema.get_columns(BronzeSchema.PAYLOADS))},  # type: ignore[typeddict-item]
+                ],
+            },
+            {
+                "name": BronzeSchema.LAUNCHPADS,
+                "endpoint": {
+                    "path": Endpoints.LAUNCHPADS,
+                },
+                "processing_steps": [
+                    {"map": keep_specific_columns(columns_to_keep=BronzeSchema.get_columns(BronzeSchema.LAUNCHPADS))},  # type: ignore[typeddict-item]
+                ],
+            },
+            {
+                "name": BronzeSchema.SHIPS,
+                "endpoint": {
+                    "path": Endpoints.SHIPS,
+                },
+                "processing_steps": [
+                    {"map": keep_specific_columns(columns_to_keep=BronzeSchema.get_columns(BronzeSchema.SHIPS))},  # type: ignore[typeddict-item]
                 ],
             },
         ],
