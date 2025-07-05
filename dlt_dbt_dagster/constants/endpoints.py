3c7dc1fe9e8f07d9911a654bb6ee233a2d2162d4
Rache Melendres <rachemelendres@gmail.com>
2025-07-05 10:59:05 +0000

    refactor(constants/endpoints): apply enums for consistency

--- dlt_dbt_dagster/constants/endpoints.py
+++ dlt_dbt_dagster/constants/endpoints.py
@@ -1,6 +1,15 @@
 # SpaceX API base URL and endpoints
+from enum import Enum
+
 BASE_URL = "https://api.spacexdata.com/v4/"
 
-RESOURCES = ["launches", "rockets", "launchpads", "payloads", "cores", "ships"]
 
-ENDPOINTS = {resource: f"{resource}/query" for resource in RESOURCES}
+class Endpoints(str, Enum):
+    """Enum for SpaceX API endpoints"""
+
+    LAUNCHES = "launches/query"
+    ROCKETS = "rockets/query"
+    LAUNCHPADS = "launchpads/query"
+    PAYLOADS = "payloads/query"
+    CORES = "cores/query"
+    SHIPS = "ships/query"
